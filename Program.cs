b5fc57cf84441d43947da7c9ae483735609e19e0
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-17 09:28:46 +0530

    stack commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push("Praveen");
+            stack.Push("Vijay");
+            stack.Push("Surya");
+            stack.Push("Ajith");
+            Console.WriteLine("The number of items are : "+stack.Count);
+            stack.Pop();
+            foreach(string items in stack)
+                Console.WriteLine(items);
+            Console.WriteLine("The Peek is " + stack.Peek()) ;
+            Console.ReadKey();
+        }
+    }
+}
